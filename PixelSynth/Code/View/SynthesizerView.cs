80361c859de2f49ec9372e124633203ce9b941f5
keiondesu96 <keiondesu96@gmail.com>
2019-10-17 03:11:49 +0300

    oscillator panel

--- PixelSynth/Code/View/SynthesizerView.cs
+++ PixelSynth/Code/View/SynthesizerView.cs
@@ -33,6 +33,31 @@ namespace PixelSynth.Code.View
             DrawOctaveAt(spriteBatch, synthsPos + new Vector2(6, 158), -1);
             DrawOctaveAt(spriteBatch, synthsPos + new Vector2(6, 95), -1);
             DrawOctaveAt(spriteBatch, synthsPos + new Vector2(6, 32), -1);
+
+            //Oscillators
+            DrawOscillatorPanel(spriteBatch, soundDriver, synthsPos + new Vector2(118, 38));
+        }
+
+        private void DrawOscillatorPanel(SpriteBatch spriteBatch, SoundDriver soundDriver, Vector2 pos)
+        {
+            if(soundDriver.CurrentBaseWave == SoundDriver.BasicWave.Sine)
+            {
+                DrawTexturePart(spriteBatch, buttonTileset, pos + new Vector2(0, 0), PixelSynth.Scale, new Rectangle(new Point(0, 228), new Point(16, 15)));
+                DrawTexturePart(spriteBatch, buttonTileset, pos + new Vector2(18, 0), PixelSynth.Scale, new Rectangle(new Point(17, 212), new Point(16, 15)));
+                DrawTexturePart(spriteBatch, buttonTileset, pos + new Vector2(36, 0), PixelSynth.Scale, new Rectangle(new Point(34, 212), new Point(16, 15)));
+            }
+            else if (soundDriver.CurrentBaseWave == SoundDriver.BasicWave.Square)
+            {
+                DrawTexturePart(spriteBatch, buttonTileset, pos + new Vector2(0, 0), PixelSynth.Scale, new Rectangle(new Point(0, 212), new Point(16, 15)));
+                DrawTexturePart(spriteBatch, buttonTileset, pos + new Vector2(18, 0), PixelSynth.Scale, new Rectangle(new Point(17, 228), new Point(16, 15)));
+                DrawTexturePart(spriteBatch, buttonTileset, pos + new Vector2(36, 0), PixelSynth.Scale, new Rectangle(new Point(34, 212), new Point(16, 15)));
+            }
+            else if (soundDriver.CurrentBaseWave == SoundDriver.BasicWave.Sawtooth)
+            {
+                DrawTexturePart(spriteBatch, buttonTileset, pos + new Vector2(0, 0), PixelSynth.Scale, new Rectangle(new Point(0, 212), new Point(16, 15)));
+                DrawTexturePart(spriteBatch, buttonTileset, pos + new Vector2(18, 0), PixelSynth.Scale, new Rectangle(new Point(17, 212), new Point(16, 15)));
+                DrawTexturePart(spriteBatch, buttonTileset, pos + new Vector2(36, 0), PixelSynth.Scale, new Rectangle(new Point(34, 228), new Point(16, 15)));
+            }
         }
 
         private void DrawOctaveAt(SpriteBatch spriteBatch, Vector2 pos, int pressedTone)
