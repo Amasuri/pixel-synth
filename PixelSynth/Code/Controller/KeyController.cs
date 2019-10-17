86e3e72c8d03c59f3be16f9eec304d18d2144b14
keiondesu96 <keiondesu96@gmail.com>
2019-10-17 04:42:59 +0300

    achievment get: unlock full power

--- PixelSynth/Code/Controller/KeyController.cs
+++ PixelSynth/Code/Controller/KeyController.cs
@@ -31,7 +31,11 @@ namespace PixelSynth.Code.Controller
             CheckChordAlterators(soundDriver);
             CheckADSR(soundDriver);
             CheckObertonator(soundDriver);
-            PlayNotes(soundDriver);
+
+            PlayLowerNotes(soundDriver);
+            PlayMediumNotes(soundDriver);
+            PlayHigherNotes(soundDriver);
+
             SwitchPresets(soundDriver);
             SwitchOscillators(soundDriver);
 
@@ -87,7 +91,7 @@ namespace PixelSynth.Code.Controller
                 soundDriver.SwitchPresetTo(Preset.SoftReverbClap);
         }
 
-        private void PlayNotes(SoundDriver soundDriver)
+        private void PlayLowerNotes(SoundDriver soundDriver)
         {
             if (keys.IsKeyDown(Keys.LeftShift) && oldKeys.IsKeyUp(Keys.LeftShift))
             {
@@ -160,6 +164,158 @@ namespace PixelSynth.Code.Controller
                 lastOctave = CurrentLowerKeysOctave;
                 lastNote = Note.Type.GSharp;
                 soundDriver.PlayPacket(Note.Type.GSharp, CurrentLowerKeysOctave);
+            }
+        }
+
+        private void PlayMediumNotes(SoundDriver soundDriver)
+        {
+            if (keys.IsKeyDown(Keys.CapsLock) && oldKeys.IsKeyUp(Keys.CapsLock))
+            {
+                lastOctave = CurrentMediumKeysOctave;
+                lastNote = Note.Type.A;
+                soundDriver.PlayPacket(Note.Type.A, CurrentMediumKeysOctave);
+            }
+            if (keys.IsKeyDown(Keys.A) && oldKeys.IsKeyUp(Keys.A))
+            {
+                lastOctave = CurrentMediumKeysOctave;
+                lastNote = Note.Type.ASharp;
+                soundDriver.PlayPacket(Note.Type.ASharp, CurrentMediumKeysOctave);
+            }
+            if (keys.IsKeyDown(Keys.S) && oldKeys.IsKeyUp(Keys.S))
+            {
+                lastOctave = CurrentMediumKeysOctave;
+                lastNote = Note.Type.B;
+                soundDriver.PlayPacket(Note.Type.B, CurrentMediumKeysOctave);
+            }
+            if (keys.IsKeyDown(Keys.D) && oldKeys.IsKeyUp(Keys.D))
+            {
+                lastOctave = CurrentMediumKeysOctave;
+                lastNote = Note.Type.C;
+                soundDriver.PlayPacket(Note.Type.C, CurrentMediumKeysOctave);
+            }
+            if (keys.IsKeyDown(Keys.F) && oldKeys.IsKeyUp(Keys.F))
+            {
+                lastOctave = CurrentMediumKeysOctave;
+                lastNote = Note.Type.CSharp;
+                soundDriver.PlayPacket(Note.Type.CSharp, CurrentMediumKeysOctave);
+            }
+            if (keys.IsKeyDown(Keys.G) && oldKeys.IsKeyUp(Keys.G))
+            {
+                lastOctave = CurrentMediumKeysOctave;
+                lastNote = Note.Type.D;
+                soundDriver.PlayPacket(Note.Type.D, CurrentMediumKeysOctave);
+            }
+            if (keys.IsKeyDown(Keys.H) && oldKeys.IsKeyUp(Keys.H))
+            {
+                lastOctave = CurrentMediumKeysOctave;
+                lastNote = Note.Type.DSharp;
+                soundDriver.PlayPacket(Note.Type.DSharp, CurrentMediumKeysOctave);
+            }
+            if (keys.IsKeyDown(Keys.J) && oldKeys.IsKeyUp(Keys.J))
+            {
+                lastOctave = CurrentMediumKeysOctave;
+                lastNote = Note.Type.E;
+                soundDriver.PlayPacket(Note.Type.E, CurrentMediumKeysOctave);
+            }
+            if (keys.IsKeyDown(Keys.K) && oldKeys.IsKeyUp(Keys.K))
+            {
+                lastOctave = CurrentMediumKeysOctave;
+                lastNote = Note.Type.F;
+                soundDriver.PlayPacket(Note.Type.F, CurrentMediumKeysOctave);
+            }
+            if (keys.IsKeyDown(Keys.L) && oldKeys.IsKeyUp(Keys.L))
+            {
+                lastOctave = CurrentMediumKeysOctave;
+                lastNote = Note.Type.FSharp;
+                soundDriver.PlayPacket(Note.Type.FSharp, CurrentMediumKeysOctave);
+            }
+            if (keys.IsKeyDown(Keys.OemSemicolon) && oldKeys.IsKeyUp(Keys.OemSemicolon))
+            {
+                lastOctave = CurrentMediumKeysOctave;
+                lastNote = Note.Type.G;
+                soundDriver.PlayPacket(Note.Type.G, CurrentMediumKeysOctave);
+            }
+            if (keys.IsKeyDown(Keys.OemQuotes) && oldKeys.IsKeyUp(Keys.OemQuotes))
+            {
+                lastOctave = CurrentMediumKeysOctave;
+                lastNote = Note.Type.GSharp;
+                soundDriver.PlayPacket(Note.Type.GSharp, CurrentMediumKeysOctave);
+            }
+        }
+
+        private void PlayHigherNotes(SoundDriver soundDriver)
+        {
+            if (keys.IsKeyDown(Keys.Tab) && oldKeys.IsKeyUp(Keys.Tab))
+            {
+                lastOctave = CurrentHigherKeysOctave;
+                lastNote = Note.Type.A;
+                soundDriver.PlayPacket(Note.Type.A, CurrentHigherKeysOctave);
+            }
+            if (keys.IsKeyDown(Keys.Q) && oldKeys.IsKeyUp(Keys.Q))
+            {
+                lastOctave = CurrentHigherKeysOctave;
+                lastNote = Note.Type.ASharp;
+                soundDriver.PlayPacket(Note.Type.ASharp, CurrentHigherKeysOctave);
+            }
+            if (keys.IsKeyDown(Keys.W) && oldKeys.IsKeyUp(Keys.W))
+            {
+                lastOctave = CurrentHigherKeysOctave;
+                lastNote = Note.Type.B;
+                soundDriver.PlayPacket(Note.Type.B, CurrentHigherKeysOctave);
+            }
+            if (keys.IsKeyDown(Keys.E) && oldKeys.IsKeyUp(Keys.E))
+            {
+                lastOctave = CurrentHigherKeysOctave;
+                lastNote = Note.Type.C;
+                soundDriver.PlayPacket(Note.Type.C, CurrentHigherKeysOctave);
+            }
+            if (keys.IsKeyDown(Keys.R) && oldKeys.IsKeyUp(Keys.R))
+            {
+                lastOctave = CurrentHigherKeysOctave;
+                lastNote = Note.Type.CSharp;
+                soundDriver.PlayPacket(Note.Type.CSharp, CurrentHigherKeysOctave);
+            }
+            if (keys.IsKeyDown(Keys.T) && oldKeys.IsKeyUp(Keys.T))
+            {
+                lastOctave = CurrentHigherKeysOctave;
+                lastNote = Note.Type.D;
+                soundDriver.PlayPacket(Note.Type.D, CurrentHigherKeysOctave);
+            }
+            if (keys.IsKeyDown(Keys.Y) && oldKeys.IsKeyUp(Keys.Y))
+            {
+                lastOctave = CurrentHigherKeysOctave;
+                lastNote = Note.Type.DSharp;
+                soundDriver.PlayPacket(Note.Type.DSharp, CurrentHigherKeysOctave);
+            }
+            if (keys.IsKeyDown(Keys.U) && oldKeys.IsKeyUp(Keys.U))
+            {
+                lastOctave = CurrentHigherKeysOctave;
+                lastNote = Note.Type.E;
+                soundDriver.PlayPacket(Note.Type.E, CurrentHigherKeysOctave);
+            }
+            if (keys.IsKeyDown(Keys.I) && oldKeys.IsKeyUp(Keys.I))
+            {
+                lastOctave = CurrentHigherKeysOctave;
+                lastNote = Note.Type.F;
+                soundDriver.PlayPacket(Note.Type.F, CurrentHigherKeysOctave);
+            }
+            if (keys.IsKeyDown(Keys.O) && oldKeys.IsKeyUp(Keys.O))
+            {
+                lastOctave = CurrentHigherKeysOctave;
+                lastNote = Note.Type.FSharp;
+                soundDriver.PlayPacket(Note.Type.FSharp, CurrentHigherKeysOctave);
+            }
+            if (keys.IsKeyDown(Keys.P) && oldKeys.IsKeyUp(Keys.P))
+            {
+                lastOctave = CurrentHigherKeysOctave;
+                lastNote = Note.Type.G;
+                soundDriver.PlayPacket(Note.Type.G, CurrentHigherKeysOctave);
+            }
+            if (keys.IsKeyDown(Keys.OemOpenBrackets) && oldKeys.IsKeyUp(Keys.OemOpenBrackets))
+            {
+                lastOctave = CurrentHigherKeysOctave;
+                lastNote = Note.Type.GSharp;
+                soundDriver.PlayPacket(Note.Type.GSharp, CurrentHigherKeysOctave);
             }
         }
     }
